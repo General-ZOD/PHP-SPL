ecb9b53612824f32e600dc273d80e8a9708cc8c2
general-zod <samuel.renegade@gmail.com>
2013-08-21 09:22:03 -0500

    Just added a CSS arrow tut

--- soap-client.php
+++ soap-client.php
@@ -36,6 +36,36 @@ $client = new SoapClient(NULL, $options);
   h2{ font-family: 'Merriweather', serif; font-weight: 200; color: #222; text-decoration: underline;}
   #span{width:300px; height:100px; background:#ccc; border:solid 2px #999; border-radius:4px; 
   font-family: 'Merriweather', serif; font-weight: 150; display: block;}
+
+  div#arrow_up{width:0; height:0; border-left: 15px solid transparent; 
+  border-right: 15px solid transparent; border-bottom: 15px solid #2f2f2f;
+  font-size: 0; line-height: 0;}
+  div#arrow_down{width:0; height:0; border-left: 15px solid transparent; 
+  border-right: 15px solid transparent; border-top: 15px solid #ccc;
+  font-size: 0; line-height: 0;}
+  p#arrow_left{width:0; height:0; border-top: 20px solid transparent; 
+  border-bottom: 20px solid transparent; border-right: 20px solid #ccc;line-height: 0;}
+  	 
+  div.tip{background: #eee; border: 1px solid #ccc; padding: 10px;
+   border-radius: 8px; box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2), 
+   position: relative; width: 200px; margin: 0 auto;}	 
+  /* Left arrow */ 
+   div.tip:before{position: absolute; 
+     border-top: 7px solid transparent; border-bottom: 7px solid transparent;
+     border-right: 7px solid #eee; border-right-color: rga(0,0,0, 0.2);
+     left: -6px; top: 20px; content: "";}
+     
+   /* Top arrow */  
+   div.tip:before{position: absolute; 
+     border-left: 7px solid transparent; border-right: 7px solid transparent;
+     border-bottom: 7px solid #eee; border-bottom-color: rga(0,0,0, 0.2);
+     top: -13px; left: 10px; content: "";}     
+    
+   /* Right arrow */  
+   div.tip:after{position: absolute; 
+     border-top: 6px solid transparent; border-bottom: 6px solid transparent;
+     border-left: 6px solid #eee; border-right-color: rga(0,0,0, 0.2);
+     right: -7px; top: 21px; content: "";}    
 </style>
 <h2>XML-RPC</h2>
   <?php print $client->getDisplayName("General", "ZOD"); ?>
@@ -67,5 +97,14 @@ $context = stream_context_create($options);
 $result = file_get_contents("http://localhost:84/json.php", 0, $context);
 print $result;
 ?>  
+<div id="arrow_up"></div>
+<div id="arrow_down"></div>
+<p id="arrow_left"></p>
+
+<div id="position:relative;" style="position: relative; width:220px; margin: 0 auto;">
+ <div class="tip">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur quam a sapien egestas eget scelerisque lectus tempor. Duis placerat tellus at erat pellentesque nec ultricies erat molestie. Integer nec orci id tortor molestie porta. Suspendisse eu sagittis quam.
+ </div>
+</div>
  </body>
 </html>
